48bcb899f5734920a9b0f1fbfb00ba01f4fae203
Alessandro Migliardi <ale.migliardi@gmail.com>
2021-03-06 13:14:16 +0100

    made it possible to send events to multiple repos

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 """
 telegram-github-webhook (or tgw) is a simple Flask + python-telegram-bot microservice made to
 relay github updates in Telegram chats. It will require a telegram bot and a publicly accessible IP
-(where github can send updates). You should configure your repo:chat_id association inside
+(where github can send updates). You should configure your repo:[chats_id] association inside
 config.json, together with the bot token.
 """
 import logging, json, io, os
@@ -15,6 +15,13 @@ with open("config.json") as f:
 logger = logging.getLogger()
 bot = Bot(config["token"])
 app = Flask(__name__)  # Standard Flask app
+
+def send_event(target, msg, doc=None):
+	for t in target:
+		if doc:
+			bot.sendDocument(chat_id=t, document=doc, caption=msg, parse_mode=ParseMode.HTML)
+		else:
+			bot.send_message(t, msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
 
 @app.route("/", methods=["GET"])
 def landing():
@@ -32,57 +39,57 @@ def github_event():
 			for commit in data["commits"]:
 				out += (f"→ <code>{commit['author']['username']}</code> {commit['message']} " +
 						f"[<a href=\"{commit['url']}\">{commit['id'][:7]}</a>]\n")
-			bot.send_message(target, out, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+			send_event(target, out)
 		elif "hook" in data: # New webhook or maybe just the initial event?
 			out = (f"<b>{data['repository']['full_name']}</b> | <i>new hook</i>\n" +
 				   f"→ <u>{data['hook']['config']['url']}</u> [{','.join(data['hook']['events'])}]")
-			bot.send_message(target, out, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+			send_event(target, out)
 		elif "member" in data:
 			if data["action"] == "added":
 				out =  (f"<b>{data['repository']['full_name']}</b> | <i>new collaborator</i>\n" +
 						f"→ <u><a href=\"{data['member']['html_url']}\">{data['member']['login']}</a></u>")
-				bot.send_message(target, out, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+				send_event(target, out)
 			else:
 				logger.error(" * [!] Not prepared to handle action \"%s\"\n> %s", data['action'], str(data))
 				text = f"<b>{data['repository']['full_name']}</b> | <i>unmapped member event</i>"
 				out = io.BytesIO(json.dumps(data, indent=2).encode('utf-8'))
 				out.name = "member-event.json"
-				bot.sendDocument(chat_id=target, document=out, caption=text, parse_mode=ParseMode.HTML)
+				send_event(target, text, out)
 		elif "issue" in data: # Something happened in Issues
 			if data["action"] == "opened":
 				out =  (f"<b>{data['repository']['full_name']}</b> | <i>opened issue</i>\n" +
 						f"→ <u><a href=\"{data['issue']['url']}\">#{data['issue']['number']}" +
 						f"</a></u> <b>{data['issue']['title']}</b> {data['issue']['body']}")
-				bot.send_message(target, out, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+				send_event(target, out)
 			elif data["action"] == "labeled":
 				labels = ",".join([ l["name"] for l in data["issue"]["labels"] ])
 				out =  (f"<b>{data['repository']['full_name']}</b> | <i>labeled issue</i>\n" +
 						f"→ <u><a href=\"{data['issue']['url']}\">#{data['issue']['number']}" +
 						f"</a></u> <b>{data['issue']['title']}</b> <u>[{labels}]</u>")
-				bot.send_message(target, out, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+				send_event(target, out)
 			elif data["action"] == "created" and "comment" in data:
 				out =  (f"<b>{data['repository']['full_name']}</b> | <i>new issue comment</i>\n" +
 						f"→ <u><a href=\"{data['issue']['url']}\">#{data['issue']['number']}" +
 						f"</a></u> <code>{data['comment']['user']['login']}</code> : {data['comment']['body']}")
-				bot.send_message(target, out, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+				send_event(target, out)
 			else:
 				logger.error(" * [!] Not prepared to handle action \"%s\"\n> %s", data['action'], str(data))
 				text = f"<b>{data['repository']['full_name']}</b> | <i>unmapped issue event</i>"
 				out = io.BytesIO(json.dumps(data, indent=2).encode('utf-8'))
 				out.name = "issue-event.json"
-				bot.sendDocument(chat_id=target, document=out, caption=text, parse_mode=ParseMode.HTML)
+				send_event(target, text, out)
 		elif "comment" in data: # New comment on commit or maybe on anything?
 			out =  (f"<b>{data['repository']['full_name']}</b> | " +
 					f"<code>{data['issue']['user']['login']}</code> <i>new comment</i>\n" +
 					f"→ [<a href=\"{data['comment']['url']}\">{data['comment']['commit_id'][:7]}</a>] " +
 					f"<code>{data['comment']['user']['login']}</code> : {data['comment']['body']}")
-			bot.send_message(target, out, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+			send_event(target, out)
 		else: # Don't know (yet) what to do with this
 			logger.error(" * [!] Not prepared to handle update\n> %s", str(data))
 			text = f"<b>{data['repository']['full_name']}</b> | <i>unmapped event</i>"
 			out = io.BytesIO(json.dumps(data, indent=2).encode('utf-8'))
 			out.name = "event.json"
-			bot.sendDocument(chat_id=target, document=out, caption=text, parse_mode=ParseMode.HTML)
+			send_event(target, text, out)
 	else:
 		logger.warning(" * Received event from unmapped repository\n> %s", str(data))
 	return "OK"
